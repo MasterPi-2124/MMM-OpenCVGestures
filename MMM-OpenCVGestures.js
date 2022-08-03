b0fe1d03e4506e7fc2d9b57a13652c8fdeb0072b
Master Pi <minh.vln140501@gmail.com>
2022-08-03 13:56:17 +0700

    check divices

--- MMM-OpenCVGestures.js
+++ MMM-OpenCVGestures.js
@@ -1,26 +1,62 @@
-Module.register('MMM-OpenCVGestures', {
-
-	// init connection to server role and setup compliment module hiding/showing upon
-	// events
-	start: function () {
-
-		Log.info('MMM-OpenCVGestures start invoked.');
-
-		// notifications are only received once the client (this file) sends the first message to the server (node_helper.js)
-		this.sendSocketNotification('INIT');
-
-	},
+Module.register("MMM-OpenCVGestures", {
 
   // Default module config.
   defaults: {
-    text: "Hello World!",
+    message: "Hello World!",
+    file: ""
+  },
+
+	start: function () {
+		Log.info('MMM-OpenCVGestures start invoked.');
+    // file = 
+    var timer = setInterval(() => {
+      this.checkCompatibility(); // cant run on server side
+      this.readFromFile(file);
+    }, 5000)
+	},
+
+  checkCompatibility: function () {
+    let onlyHas = [];
+    navigator.mediaDevices.enumerateDevices()
+    .then(devices => {
+      let video = audio = false;
+      devices.forEach(device => {
+        if (device.kind == "audioinput") {
+          onlyHas.push(device.kind);
+          audio = true;
+        } else if (device.kind == "videoinput") {
+          onlyHas.push(device.kind);
+          video = true;
+        }
+      });
+      
+      if (video == true && audio == true) {
+        console.log("camera and audio detected");
+        this.config.message = "all devices OK";
+        console.log(this.config.message);
+        this.updateDom();
+      } else {
+        console.log("camera and audio not work");
+        Log.log(onlyHas);
+        this.config.message = "all devices not OK";
+        console.log(this.config.message);
+        this.updateDom();
+      }
+    })
+    .catch(function(err) {dfgdfg
+      console.log(err.name + ": " + err.message);
+    });
   },
 
   // Override dom generator.
-  getDom: function () {
+  getDom() {
     var wrapper = document.createElement("div");
-    wrapper.innerHTML = this.config.text;
+    wrapper.innerHTML = this.config.message;
     return wrapper;
+  },
+
+  socketNotificationReceived(notification, payload) {
+    Log.log("ahihi");
   },
 
 });
