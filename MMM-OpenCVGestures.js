948e6994284ab3b431b00f326f66c7cbd99c0ddd
Master Pi <minh.vln140501@gmail.com>
2022-08-07 21:33:16 +0700

    fix hide message

--- MMM-OpenCVGestures.js
+++ MMM-OpenCVGestures.js
@@ -7,7 +7,7 @@ Module.register("MMM-OpenCVGestures", {
     customCommand: {},
     hidden: false,
     GPIO: -1, // GPIO Pin of PIR sensor
-    standbyTime: 10 * 1000, // default duration time before screen off
+    standbyTime: 300 * 1000, // default duration time before screen off
   },
 
   getStyles: function () {
@@ -29,13 +29,15 @@ Module.register("MMM-OpenCVGestures", {
     thisModule = MM.getModules().withClass(this.name);
     if (thisModule && thisModule.length === 1) {
       var module = thisModule[0];
-      if (state === true) {
-      console.log("[OP]: MMM-OpenCVGestures hidden.");
-      module.hide(this.config.fadeInterval);
-      } else {
-      console.log("[OP]: MMM-OpenCVGestures shown.");
-      module.show(this.config.fadeInterval);
-      }
+      if (state === true && this.config.hidden === false) {
+        console.log("[OP]: MMM-OpenCVGestures hidden.");
+        module.hide(this.config.fadeInterval);
+        this.config.hidden === true;
+      } else if (state === false && this.config.hidden === true) {
+        console.log("[OP]: MMM-OpenCVGestures shown.");
+        module.show(this.config.fadeInterval);
+        this.config.hidden === false;
+    }
     }
   },
 
